a83ae778724a51c4ee0bb06d308e794d6fe4b688
HP <HP@DESKTOP-CPLQTK2>
2023-10-10 20:01:07 +0530

    SpringBoot JPA ManyToOne Unidirectional Mapping

---
+++ src/main/java/com/springboot/SpringBootManyToOneUnidirectionalMappingDirectMethodApplication.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootManyToOneUnidirectionalMappingDirectMethodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootManyToOneUnidirectionalMappingDirectMethodApplication.class, args);
+	}
+
+}
